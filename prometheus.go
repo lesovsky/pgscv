4989133daaa9a31239507a73c4f7c5e1976ed433
Alexey Lesovsky <lesovsky@gmail.com>
2019-03-23 10:50:26 +0500

    added pg_schema_redundant_index_bytes

--- prometheus.go
+++ prometheus.go
@@ -115,6 +115,7 @@ var (
 		{Name: "pg_schema_non_pk_table", Query: pgSchemaNonPrimaryKeyTablesQuery, ValueNames: []string{"exists"}, LabelNames: []string{"datname", "schemaname", "relname"}, Schedule: Schedule{Interval: 5 * time.Minute}},
 		{Name: "pg_schema_invalid_index", Query: pgSchemaInvalidIndexesQuery, ValueNames: []string{"exists"}, LabelNames: []string{"datname", "schemaname", "relname", "indexrelname"}, Schedule: Schedule{Interval: 5 * time.Minute}},
 		{Name: "pg_schema_non_indexed_fkey", Query: pgSchemaNonIndexedFKQuery, ValueNames: []string{"exists"}, LabelNames: []string{"datname", "schemaname", "relname", "colnames", "constraint", "referenced"}, Schedule: Schedule{Interval: 5 * time.Minute}},
+		{Name: "pg_schema_redundant_index", Query: pgSchemaRedundantIndexesQuery, ValueNames: []string{"bytes"}, LabelNames: []string{"datname", "schemaname", "relname", "index", "redundant"}, Schedule: Schedule{Interval: 5 * time.Minute}},
 		// system metrics are always oneshot, there is no 'database' entity
 		{Name: "node_cpu_usage", Stype: stypeSystem, ValueNames: []string{"time"}, LabelNames: []string{"mode"}},
 		{Name: "node_diskstats", Stype: stypeSystem, ValueNames: diskstatsValueNames, LabelNames: []string{"device"}},
