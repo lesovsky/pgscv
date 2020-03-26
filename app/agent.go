37db058625af34fe67a36c67b83fc4d093adc7e5
Alexey Lesovsky <lesovsky@gmail.com>
2020-03-26 11:20:54 +0500

    - by default use a machine-id for job label, - use separated pushes for services

--- app/agent.go
+++ app/agent.go
@@ -6,8 +6,10 @@ import (
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"github.com/prometheus/client_golang/prometheus/push"
 	"github.com/rs/zerolog"
+	"io/ioutil"
 	"net/http"
 	"os"
+	"strings"
 	"time"
 )
 
@@ -45,44 +47,84 @@ func Start(c *Config) error {
 	}
 
 	logger.Info().Msgf("use PUSH model, sending metrics to %s every %d seconds", c.MetricServiceBaseURL, c.MetricsSendInterval/time.Second)
-	hostname, err := os.Hostname()
-	if err != nil {
-		return err
-	}
 
 	// A job label is the special one which provides metrics uniqueness across several hosts and guarantees metrics will
 	// not be overwritten on Pushgateway side. There is no other use-cases for this label, hence before ingesting by Prometheus
 	// this label should be removed with 'metric_relabel_config' rule.
-	var jobLabel = "db_system_" + fmt.Sprintf("%x", md5.Sum([]byte(hostname)))
+	jobLabelBase, err := getJobLabelBase(logger)
+	if err != nil {
+		return err
+	}
 
 	for {
 		logger.Debug().Msgf("start job")
 		var start = time.Now()
 
-		var pusher = push.New(c.MetricServiceBaseURL, jobLabel)
-
-		// if api-key specified use custom http-client and attach api-key to http requests
-		if c.APIKey != "" {
-			client := newHTTPClient(c.APIKey)
-			pusher.Client(client)
-		}
-
-		// collect metrics for all discovered services
+		// metrics for every discovered service is wrapped into a separate push
 		for _, service := range instanceRepo.Services {
-			pusher.Collector(service.Exporter)
-		}
+			jobLabel := fmt.Sprintf("db_system_%s_%s", jobLabelBase, service.ServiceID)
+			var pusher = push.New(c.MetricServiceBaseURL, jobLabel)
 
-		// push metrics
-		if err := pusher.Add(); err != nil {
-			// it is not critical error, just show it and continue
-			logger.Warn().Err(err).Msg("could not push metrics")
+			// if api-key specified use custom http-client and attach api-key to http requests
+			if c.APIKey != "" {
+				client := newHTTPClient(c.APIKey)
+				pusher.Client(client)
+			}
+
+			// collect metrics for all discovered services
+			pusher.Collector(service.Exporter)
+
+			// push metrics
+			if err := pusher.Add(); err != nil {
+				// it is not critical error, just show it and continue
+				logger.Warn().Err(err).Msg("could not push metrics")
+			}
 		}
 
 		// sleep now
-		logger.Debug().Msg("job is finished, going to sleep")
+		logger.Debug().Msg("all jobs are finished, going to sleep")
 		time.Sleep(time.Until(start.Add(c.MetricsSendInterval)))
 	}
 
 	// TODO: тупиковый for сверху, из него никак не выйти
 	//return nil
+}
+
+// getJobLabelBase returns a unique string for job label. The string is based on machine-id or hostname.
+func getJobLabelBase(logger zerolog.Logger) (string, error) {
+	// try to use machine-id-based label
+	machineID, err := getLabelByMachineID()
+	if err == nil {
+		return machineID, nil
+	}
+
+	// if getting machine-id failed, try to use hostname-based label
+	logger.Warn().Err(err).Msgf("read machine-id failed, fallback to use hostname")
+	machineID, err = getLabelByHostname()
+	if err != nil {
+		logger.Warn().Err(err).Msgf("get hostname failed, can't create job label")
+		return "", err
+	}
+	return machineID, nil
+}
+
+// getLabelByMachineID reads /etc/machine-id and return its content
+func getLabelByMachineID() (string, error) {
+	if _, err := os.Stat("/etc/machine-id"); os.IsNotExist(err) {
+		return "", err
+	}
+	content, err := ioutil.ReadFile("/etc/machine-id")
+	if err != nil {
+		return "", err
+	}
+	return strings.TrimSpace(string(content)), nil
+}
+
+// getLabelByHostname gets hostname and hashes it using MD5 and returns the hash
+func getLabelByHostname() (string, error) {
+	hostname, err := os.Hostname()
+	if err != nil {
+		return "", err
+	}
+	return fmt.Sprintf("%x", md5.Sum([]byte(hostname))), nil
 }
