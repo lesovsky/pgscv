cad3fb044566d743e146ed9b87a829c9414828ea
lesovsky <lesovsky@gmail.com>
2020-09-05 09:51:11 +0500

    Add tests.

---
+++ internal/collector/collector_test.go
@@ -0,0 +1,34 @@
+package collector
+
+import (
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestPgscvCollector_Collect(t *testing.T) {
+	// Create test stuff - factory and collector, register system only metrics.
+	f := Factories{}
+	f.RegisterSystemCollectors()
+	c, err := NewPgscvCollector("0", "test:0", f, Config{})
+	assert.NoError(t, err)
+	assert.NotNil(t, c)
+
+	// Create channel and run Collect method which collect metrics and transmit them into channel.
+	ch := make(chan prometheus.Metric)
+
+	go func() {
+		c.Collect(ch)
+		close(ch)
+	}()
+
+	// Catch metrics until channel is opened.
+	var metrics []prometheus.Metric
+	for m := range ch {
+		metrics = append(metrics, m)
+	}
+
+	// Check metrics slice should not be nil or empty.
+	assert.NotNil(t, metrics)
+	assert.Greater(t, len(metrics), 0)
+}
