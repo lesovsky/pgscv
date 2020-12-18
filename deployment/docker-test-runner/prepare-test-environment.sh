813178a2cae9f12dfe82c4903f25ccc8c6c4d725
lesovsky <lesovsky@gmail.com>
2020-12-18 11:03:54 +0500

    Update dependencies of barcodepro/weaponry-pgscv-test-runner.

--- deployment/docker-test-runner/prepare-test-environment.sh
+++ deployment/docker-test-runner/prepare-test-environment.sh
@@ -4,7 +4,7 @@ MAIN_DATADIR=/var/lib/postgresql/data/main
 STDB_DATADIR=/var/lib/postgresql/data/standby
 
 # init postgres
-su - postgres -c "/usr/lib/postgresql/12/bin/initdb -k -E UTF8 --locale=en_US.UTF-8 -D ${MAIN_DATADIR}"
+su - postgres -c "/usr/lib/postgresql/13/bin/initdb -k -E UTF8 --locale=en_US.UTF-8 -D ${MAIN_DATADIR}"
 
 # add extra config parameters
 echo "ssl = on" >> ${MAIN_DATADIR}/postgresql.auto.conf
@@ -18,7 +18,7 @@ echo "shared_preload_libraries = 'pg_stat_statements'" >> ${MAIN_DATADIR}/postgr
 echo "host all  pgscv 127.0.0.1/32 trust" >> ${MAIN_DATADIR}/pg_hba.conf
 
 # run main postgres
-su - postgres -c "/usr/lib/postgresql/12/bin/pg_ctl -w -t 30 -l /var/run/postgresql/startup-main.log -D ${MAIN_DATADIR} start"
+su - postgres -c "/usr/lib/postgresql/13/bin/pg_ctl -w -t 30 -l /var/run/postgresql/startup-main.log -D ${MAIN_DATADIR} start"
 su - postgres -c "psql -c \"SELECT pg_create_physical_replication_slot('standby_test_slot')\""
 
 # run standby postgres
@@ -26,7 +26,7 @@ su - postgres -c "pg_basebackup -P -R -X stream -c fast -h 127.0.0.1 -p 5432 -U 
 echo "port = 5433" >> ${STDB_DATADIR}/postgresql.auto.conf
 echo "primary_slot_name = 'standby_test_slot'" >> ${STDB_DATADIR}/postgresql.auto.conf
 echo "log_filename = 'postgresql-standby.log'" >> ${STDB_DATADIR}/postgresql.auto.conf
-su - postgres -c "/usr/lib/postgresql/12/bin/pg_ctl -w -t 30 -l /var/run/postgresql/startup-standby.log -D ${STDB_DATADIR} start"
+su - postgres -c "/usr/lib/postgresql/13/bin/pg_ctl -w -t 30 -l /var/run/postgresql/startup-standby.log -D ${STDB_DATADIR} start"
 
 # add fixtures, tiny workload
 su - postgres -c 'psql -f /usr/local/testing/fixtures.sql'
@@ -45,4 +45,5 @@ su - postgres -c "/usr/sbin/pgbouncer -d /etc/pgbouncer/pgbouncer.ini"
 
 # check services availability
 pg_isready -t 10 -h 127.0.0.1 -p 5432 -U pgscv -d postgres
+pg_isready -t 10 -h 127.0.0.1 -p 5433 -U pgscv -d postgres
 pg_isready -t 10 -h 127.0.0.1 -p 6432 -U pgscv -d pgbouncer
