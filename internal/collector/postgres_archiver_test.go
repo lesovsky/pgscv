fa5479d8a542ee3aa51e07df4bfba9f99d0b8db4
Alexey Lesovsky <lesovsky@gmail.com>
2021-05-15 21:06:32 +0500

    Revert "Use updated common collector logic in postgres/replication collector."
    This reverts commit 9ff3a508

--- internal/collector/postgres_archiver_test.go
+++ internal/collector/postgres_archiver_test.go
@@ -24,49 +24,49 @@ func TestPostgresWalArchivingCollector_Update(t *testing.T) {
 	pipeline(t, input)
 }
 
-//func Test_parsePostgresWalArchivingStats(t *testing.T) {
-//	var testCases = []struct {
-//		name string
-//		res  *model.PGResult
-//		want postgresWalArchivingStat
-//	}{
-//		{
-//			name: "normal output",
-//			res: &model.PGResult{
-//				Nrows: 1,
-//				Ncols: 4,
-//				Colnames: []pgproto3.FieldDescription{
-//					{Name: []byte("archived_count")}, {Name: []byte("failed_count")},
-//					{Name: []byte("since_last_archive_seconds")}, {Name: []byte("lag_bytes")},
-//				},
-//				Rows: [][]sql.NullString{
-//					{
-//						{String: "4587", Valid: true}, {String: "0", Valid: true},
-//						{String: "17", Valid: true}, {String: "12345678", Valid: true},
-//					},
-//				},
-//			},
-//			want: postgresWalArchivingStat{archived: 4587, failed: 0, sinceArchivedSeconds: 17, lagBytes: 12345678},
-//		},
-//		{
-//			name: "no rows output",
-//			res: &model.PGResult{
-//				Nrows: 0,
-//				Ncols: 5,
-//				Colnames: []pgproto3.FieldDescription{
-//					{Name: []byte("archived_count")}, {Name: []byte("failed_count")},
-//					{Name: []byte("since_last_archive_seconds")}, {Name: []byte("lag_bytes")},
-//				},
-//				Rows: [][]sql.NullString{},
-//			},
-//			want: postgresWalArchivingStat{archived: 0, failed: 0, sinceArchivedSeconds: 0, lagBytes: 0},
-//		},
-//	}
-//
-//	for _, tc := range testCases {
-//		t.Run(tc.name, func(t *testing.T) {
-//			got := parsePostgresWalArchivingStats(tc.res)
-//			assert.EqualValues(t, tc.want, got)
-//		})
-//	}
-//}
+func Test_parsePostgresWalArchivingStats(t *testing.T) {
+	var testCases = []struct {
+		name string
+		res  *model.PGResult
+		want postgresWalArchivingStat
+	}{
+		{
+			name: "normal output",
+			res: &model.PGResult{
+				Nrows: 1,
+				Ncols: 4,
+				Colnames: []pgproto3.FieldDescription{
+					{Name: []byte("archived_count")}, {Name: []byte("failed_count")},
+					{Name: []byte("since_last_archive_seconds")}, {Name: []byte("lag_bytes")},
+				},
+				Rows: [][]sql.NullString{
+					{
+						{String: "4587", Valid: true}, {String: "0", Valid: true},
+						{String: "17", Valid: true}, {String: "12345678", Valid: true},
+					},
+				},
+			},
+			want: postgresWalArchivingStat{archived: 4587, failed: 0, sinceArchivedSeconds: 17, lagBytes: 12345678},
+		},
+		{
+			name: "no rows output",
+			res: &model.PGResult{
+				Nrows: 0,
+				Ncols: 5,
+				Colnames: []pgproto3.FieldDescription{
+					{Name: []byte("archived_count")}, {Name: []byte("failed_count")},
+					{Name: []byte("since_last_archive_seconds")}, {Name: []byte("lag_bytes")},
+				},
+				Rows: [][]sql.NullString{},
+			},
+			want: postgresWalArchivingStat{archived: 0, failed: 0, sinceArchivedSeconds: 0, lagBytes: 0},
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			got := parsePostgresWalArchivingStats(tc.res)
+			assert.EqualValues(t, tc.want, got)
+		})
+	}
+}
