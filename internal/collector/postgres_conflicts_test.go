be53e03821e3fa9001113001a93bb4dcaf739092
Alexey Lesovsky <lesovsky@gmail.com>
2021-05-15 10:19:57 +0500

    Use updated common collector logic in postgres/conflicts collector.

--- internal/collector/postgres_conflicts_test.go
+++ internal/collector/postgres_conflicts_test.go
@@ -1,16 +1,13 @@
 package collector
 
 import (
-	"database/sql"
-	"github.com/jackc/pgproto3/v2"
-	"github.com/stretchr/testify/assert"
 	"github.com/weaponry/pgscv/internal/model"
 	"testing"
 )
 
 func TestPostgresConflictsCollector_Update(t *testing.T) {
 	var input = pipelineInput{
-		optional: []string{
+		required: []string{
 			"postgres_recovery_conflicts_total",
 		},
 		collector: NewPostgresConflictsCollector,
@@ -18,44 +15,4 @@ func TestPostgresConflictsCollector_Update(t *testing.T) {
 	}
 
 	pipeline(t, input)
-}
-
-func Test_parsePostgresConflictsStats(t *testing.T) {
-	var testCases = []struct {
-		name string
-		res  *model.PGResult
-		want map[string]postgresConflictStat
-	}{
-		{
-			name: "normal output",
-			res: &model.PGResult{
-				Nrows: 2,
-				Ncols: 6,
-				Colnames: []pgproto3.FieldDescription{
-					{Name: []byte("datname")}, {Name: []byte("confl_tablespace")}, {Name: []byte("confl_lock")},
-					{Name: []byte("confl_snapshot")}, {Name: []byte("confl_bufferpin")}, {Name: []byte("confl_deadlock")},
-				},
-				Rows: [][]sql.NullString{
-					{
-						{String: "testdb1", Valid: true}, {String: "123", Valid: true}, {String: "548", Valid: true},
-						{String: "784", Valid: true}, {String: "896", Valid: true}, {String: "896", Valid: true},
-					},
-					{
-						{String: "testdb2", Valid: true}, {}, {}, {}, {}, {},
-					},
-				},
-			},
-			want: map[string]postgresConflictStat{
-				"testdb1": {datname: "testdb1", tablespace: 123, lock: 548, snapshot: 784, bufferpin: 896, deadlock: 896},
-				"testdb2": {datname: "testdb2"},
-			},
-		},
-	}
-
-	for _, tc := range testCases {
-		t.Run(tc.name, func(t *testing.T) {
-			got := parsePostgresConflictStats(tc.res, []string{"datname", "reason"})
-			assert.EqualValues(t, tc.want, got)
-		})
-	}
 }
