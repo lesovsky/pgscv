e5c7faec306b06c084da0d6cb24e6901e62a0521
Alexey Lesovsky <lesovsky@gmail.com>
2021-05-15 13:02:11 +0500

    Use updated common collector logic in postgres/database collector.

--- internal/collector/postgres_database.go
+++ internal/collector/postgres_database.go
@@ -2,213 +2,163 @@ package collector
 
 import (
 	"github.com/prometheus/client_golang/prometheus"
-	"github.com/weaponry/pgscv/internal/log"
 	"github.com/weaponry/pgscv/internal/model"
-	"github.com/weaponry/pgscv/internal/store"
-	"strconv"
 )
 
 const (
 	databaseQuery = "SELECT " +
 		"COALESCE(datname, '__shared__') AS datname, " +
-		"xact_commit, xact_rollback, blks_read, blks_hit, tup_returned, tup_fetched, tup_inserted, tup_updated, tup_deleted, " +
-		"conflicts, temp_files, temp_bytes, deadlocks, blk_read_time, blk_write_time, pg_database_size(datname) as size_bytes, " +
-		"coalesce(extract('epoch' from age(now(), stats_reset)), 0) as stats_age_seconds " +
+		"xact_commit, xact_rollback, " +
+		"blks_read, blks_hit , tup_returned, tup_fetched, tup_inserted, tup_updated, tup_deleted, " +
+		"conflicts, temp_files, temp_bytes, deadlocks, " +
+		"blk_read_time * 0.001 AS read_t, blk_write_time * 0.001 AS write_t, " +
+		"pg_database_size(datname) AS size_bytes, " +
+		"coalesce(extract('epoch' from age(now(), stats_reset)), 0) AS stats_age_seconds " +
 		"FROM pg_stat_database WHERE datname IN (SELECT datname FROM pg_database WHERE datallowconn AND NOT datistemplate)"
 
-	xidLimitQuery = "SELECT 'database' AS src, 2147483647 - greatest(max(age(datfrozenxid)), max(age(coalesce(nullif(datminmxid, 1), datfrozenxid)))) AS to_limit FROM pg_database " +
-		"UNION SELECT 'prepared_xacts' AS src, 2147483647 - coalesce(max(age(transaction)), 0) AS to_limit FROM pg_prepared_xacts " +
-		"UNION SELECT 'replication_slots' AS src, 2147483647 - greatest(coalesce(min(age(xmin)), 0), coalesce(min(age(catalog_xmin)), 0)) AS to_limit FROM pg_replication_slots"
+	xidLimitQuery = "SELECT 'database' AS xid, 2147483647 - greatest(max(age(datfrozenxid)), max(age(coalesce(nullif(datminmxid, 1), datfrozenxid)))) AS to_limit FROM pg_database " +
+		"UNION SELECT 'prepared_xacts' AS xid, 2147483647 - coalesce(max(age(transaction)), 0) AS to_limit FROM pg_prepared_xacts " +
+		"UNION SELECT 'replication_slots' AS xid, 2147483647 - greatest(coalesce(min(age(xmin)), 0), coalesce(min(age(catalog_xmin)), 0)) AS to_limit FROM pg_replication_slots"
 )
 
 type postgresDatabasesCollector struct {
-	commits    typedDesc
-	rollbacks  typedDesc
-	conflicts  typedDesc
-	deadlocks  typedDesc
-	blocks     typedDesc
-	tuples     typedDesc
-	tempbytes  typedDesc
-	tempfiles  typedDesc
-	blockstime typedDesc
-	sizes      typedDesc
-	statsage   typedDesc
-	xidlimit   typedDesc
-	labelNames []string
-	custom     []typedDescSet
+	builtin []typedDescSet
+	custom  []typedDescSet
 }
 
 // NewPostgresDatabasesCollector returns a new Collector exposing postgres databases stats.
 // For details see https://www.postgresql.org/docs/current/monitoring-stats.html#PG-STAT-DATABASE-VIEW
 func NewPostgresDatabasesCollector(constLabels prometheus.Labels, settings model.CollectorSettings) (Collector, error) {
-	// This instance of builtinSubsystems just used for detecting collisions with user-defined metrics.
-	// This must be always synchronized with metric descriptors in returned value.
 	builtinSubsystems := model.Subsystems{
 		"database": {
+			Query: databaseQuery,
 			Metrics: model.Metrics{
-				{ShortName: "datname"},
-				{ShortName: "xact_commits_total"},
-				{ShortName: "xact_rollbacks_total"},
-				{ShortName: "conflicts_total"},
-				{ShortName: "deadlocks_total"},
-				{ShortName: "blocks_total"},
-				{ShortName: "tuples_total"},
-				{ShortName: "temp_bytes_total"},
-				{ShortName: "temp_files_total"},
-				{ShortName: "blk_time_seconds"},
-				{ShortName: "size_bytes"},
-				{ShortName: "stats_age_seconds"},
+				{
+					ShortName:   "xact_commits_total",
+					Usage:       "COUNTER",
+					Value:       "xact_commit",
+					Labels:      []string{"datname"},
+					Description: "Total number of transactions had been committed.",
+				},
+				{
+					ShortName:   "xact_rollbacks_total",
+					Usage:       "COUNTER",
+					Value:       "xact_rollback",
+					Labels:      []string{"datname"},
+					Description: "Total number of transactions had been rolled back.",
+				},
+				{
+					ShortName:   "conflicts_total",
+					Usage:       "COUNTER",
+					Value:       "conflicts",
+					Labels:      []string{"datname"},
+					Description: "Total number of recovery conflicts occurred.",
+				},
+				{
+					ShortName:   "deadlocks_total",
+					Usage:       "COUNTER",
+					Value:       "deadlocks",
+					Labels:      []string{"datname"},
+					Description: "Total number of deadlocks occurred.",
+				},
+				{
+					ShortName: "blocks_total",
+					Usage:     "COUNTER",
+					LabeledValues: map[string][]string{
+						"access": {
+							"blks_read/read",
+							"blks_hit/hit",
+						},
+					},
+					Labels:      []string{"datname"},
+					Description: "Total number of disk blocks had been accessed by each type of access.",
+				},
+				{
+					ShortName: "tuples_total",
+					Usage:     "COUNTER",
+					LabeledValues: map[string][]string{
+						"op": {
+							"tup_returned/returned",
+							"tup_fetched/fetched",
+							"tup_inserted/inserted",
+							"tup_updated/updated",
+							"tup_deleted/deleted",
+						},
+					},
+					Labels:      []string{"datname"},
+					Description: "Total number of rows processed by each type of operation.",
+				},
+				{
+					ShortName:   "temp_files_total",
+					Usage:       "COUNTER",
+					Value:       "temp_files",
+					Labels:      []string{"datname"},
+					Description: "Total number of temporary files created by queries.",
+				},
+				{
+					ShortName:   "temp_bytes_total",
+					Usage:       "COUNTER",
+					Value:       "temp_bytes",
+					Labels:      []string{"datname"},
+					Description: "Total amount of data written to temporary files by queries.",
+				},
+				{
+					ShortName: "blk_time_seconds",
+					Usage:     "COUNTER",
+					LabeledValues: map[string][]string{
+						"type": {
+							"read_t/read",
+							"write_t/write",
+						},
+					},
+					Labels:      []string{"datname"},
+					Description: "Time spent accessing data file blocks by backends in this database in each access type, in seconds.",
+				},
+				{
+					ShortName:   "size_bytes",
+					Usage:       "GAUGE",
+					Value:       "size_bytes",
+					Labels:      []string{"datname"},
+					Description: "Total size of the database, in bytes.",
+				},
+				{
+					ShortName:   "stats_age_seconds",
+					Usage:       "COUNTER",
+					Value:       "stats_age_seconds",
+					Labels:      []string{"datname"},
+					Description: "The age of the activity statistics, in seconds.",
+				},
 			},
 		},
 		"xacts": {
+			Query: xidLimitQuery,
 			Metrics: model.Metrics{
-				{ShortName: "xid"},
-				{ShortName: "left_before_wraparound"},
+				{
+					ShortName:   "left_before_wraparound",
+					Usage:       "GAUGE",
+					Value:       "to_limit",
+					Labels:      []string{"xid"},
+					Description: "The least number of transactions (among all databases) left before force shutdown due to XID wraparound.",
+				},
 			},
 		},
 	}
 
 	removeCollisions(builtinSubsystems, settings.Subsystems)
 
-	databaseLabelNames := []string{"datname"}
-
 	return &postgresDatabasesCollector{
-		labelNames: databaseLabelNames,
-		commits: typedDesc{
-			desc: prometheus.NewDesc(
-				prometheus.BuildFQName("postgres", "database", "xact_commits_total"),
-				"Total number of transactions had been committed.",
-				databaseLabelNames, constLabels,
-			), valueType: prometheus.CounterValue,
-		},
-		rollbacks: typedDesc{
-			desc: prometheus.NewDesc(
-				prometheus.BuildFQName("postgres", "database", "xact_rollbacks_total"),
-				"Total number of transactions had been rolled back.",
-				databaseLabelNames, constLabels,
-			), valueType: prometheus.CounterValue,
-		},
-		conflicts: typedDesc{
-			desc: prometheus.NewDesc(
-				prometheus.BuildFQName("postgres", "database", "conflicts_total"),
-				"Total number of recovery conflicts occurred.",
-				databaseLabelNames, constLabels,
-			), valueType: prometheus.CounterValue,
-		},
-		deadlocks: typedDesc{
-			desc: prometheus.NewDesc(
-				prometheus.BuildFQName("postgres", "database", "deadlocks_total"),
-				"Total number of deadlocks occurred.",
-				databaseLabelNames, constLabels,
-			), valueType: prometheus.CounterValue,
-		},
-		blocks: typedDesc{
-			desc: prometheus.NewDesc(
-				prometheus.BuildFQName("postgres", "database", "blocks_total"),
-				"Total number of disk blocks had been accessed by each type of access.",
-				[]string{"datname", "access"}, constLabels,
-			), valueType: prometheus.CounterValue,
-		},
-		tuples: typedDesc{
-			desc: prometheus.NewDesc(
-				prometheus.BuildFQName("postgres", "database", "tuples_total"),
-				"Total number of rows processed by each type of operation.",
-				[]string{"datname", "op"}, constLabels,
-			), valueType: prometheus.CounterValue,
-		},
-		tempbytes: typedDesc{
-			desc: prometheus.NewDesc(
-				prometheus.BuildFQName("postgres", "database", "temp_bytes_total"),
-				"Total amount of data written to temporary files by queries.",
-				databaseLabelNames, constLabels,
-			), valueType: prometheus.CounterValue,
-		},
-		tempfiles: typedDesc{
-			desc: prometheus.NewDesc(
-				prometheus.BuildFQName("postgres", "database", "temp_files_total"),
-				"Total number of temporary files created by queries.",
-				databaseLabelNames, constLabels,
-			), valueType: prometheus.CounterValue,
-		},
-		blockstime: typedDesc{
-			desc: prometheus.NewDesc(
-				prometheus.BuildFQName("postgres", "database", "blk_time_seconds"),
-				"Time spent accessing data file blocks by backends in this database in each access type, in seconds.",
-				[]string{"datname", "type"}, constLabels,
-			), valueType: prometheus.CounterValue, factor: .001,
-		},
-		sizes: typedDesc{
-			desc: prometheus.NewDesc(
-				prometheus.BuildFQName("postgres", "database", "size_bytes"),
-				"Total size of the database, in bytes.",
-				databaseLabelNames, constLabels,
-			), valueType: prometheus.GaugeValue,
-		},
-		statsage: typedDesc{
-			desc: prometheus.NewDesc(
-				prometheus.BuildFQName("postgres", "database", "stats_age_seconds"),
-				"The age of the activity statistics, in seconds.",
-				databaseLabelNames, constLabels,
-			), valueType: prometheus.CounterValue,
-		},
-		xidlimit: typedDesc{
-			desc: prometheus.NewDesc(
-				prometheus.BuildFQName("postgres", "xacts", "left_before_wraparound"),
-				"The least number of transactions (among all databases) left before force shutdown due to XID wraparound.",
-				[]string{"xid"}, constLabels,
-			), valueType: prometheus.CounterValue,
-		},
-		custom: newDeskSetsFromSubsystems("postgres", settings.Subsystems, constLabels),
+		builtin: newDeskSetsFromSubsystems("postgres", builtinSubsystems, constLabels),
+		custom:  newDeskSetsFromSubsystems("postgres", settings.Subsystems, constLabels),
 	}, nil
 }
 
 // Update method collects statistics, parse it and produces metrics that are sent to Prometheus.
 func (c *postgresDatabasesCollector) Update(config Config, ch chan<- prometheus.Metric) error {
-	conn, err := store.New(config.ConnString)
+	// Update builtin metrics.
+	err := updateAllDescSets(config, c.builtin, ch)
 	if err != nil {
 		return err
 	}
-
-	res, err := conn.Query(databaseQuery)
-	if err != nil {
-		return err
-	}
-
-	stats := parsePostgresDatabasesStats(res, c.labelNames)
-
-	res, err = conn.Query(xidLimitQuery)
-	if err != nil {
-		return err
-	}
-
-	conn.Close()
-
-	xidStats := parsePostgresXidLimitStats(res)
-
-	for _, stat := range stats {
-		ch <- c.commits.mustNewConstMetric(stat.xactcommit, stat.datname)
-		ch <- c.rollbacks.mustNewConstMetric(stat.xactrollback, stat.datname)
-		ch <- c.conflicts.mustNewConstMetric(stat.conflicts, stat.datname)
-		ch <- c.deadlocks.mustNewConstMetric(stat.deadlocks, stat.datname)
-		ch <- c.blocks.mustNewConstMetric(stat.blksread, stat.datname, "read")
-		ch <- c.blocks.mustNewConstMetric(stat.blkshit, stat.datname, "hit")
-		ch <- c.tuples.mustNewConstMetric(stat.tupreturned, stat.datname, "returned")
-		ch <- c.tuples.mustNewConstMetric(stat.tupfetched, stat.datname, "fetched")
-		ch <- c.tuples.mustNewConstMetric(stat.tupinserted, stat.datname, "inserted")
-		ch <- c.tuples.mustNewConstMetric(stat.tupupdated, stat.datname, "updated")
-		ch <- c.tuples.mustNewConstMetric(stat.tupdeleted, stat.datname, "deleted")
-
-		ch <- c.tempbytes.mustNewConstMetric(stat.tempbytes, stat.datname)
-		ch <- c.tempfiles.mustNewConstMetric(stat.tempfiles, stat.datname)
-		ch <- c.blockstime.mustNewConstMetric(stat.blkreadtime, stat.datname, "read")
-		ch <- c.blockstime.mustNewConstMetric(stat.blkwritetime, stat.datname, "write")
-		ch <- c.sizes.mustNewConstMetric(stat.sizebytes, stat.datname)
-		ch <- c.statsage.mustNewConstMetric(stat.statsage, stat.datname)
-	}
-
-	ch <- c.xidlimit.mustNewConstMetric(xidStats.database, "pg_database")
-	ch <- c.xidlimit.mustNewConstMetric(xidStats.prepared, "pg_prepared_xacts")
-	ch <- c.xidlimit.mustNewConstMetric(xidStats.replSlot, "pg_replication_slots")
 
 	// Update user-defined metrics.
 	err = updateAllDescSets(config, c.custom, ch)
@@ -217,185 +167,4 @@ func (c *postgresDatabasesCollector) Update(config Config, ch chan<- prometheus.
 	}
 
 	return nil
-}
-
-// postgresDatabaseStat represents per-database stats based on pg_stat_database.
-type postgresDatabaseStat struct {
-	datname      string
-	xactcommit   float64
-	xactrollback float64
-	blksread     float64
-	blkshit      float64
-	tupreturned  float64
-	tupfetched   float64
-	tupinserted  float64
-	tupupdated   float64
-	tupdeleted   float64
-	conflicts    float64
-	tempfiles    float64
-	tempbytes    float64
-	deadlocks    float64
-	blkreadtime  float64
-	blkwritetime float64
-	sizebytes    float64
-	statsage     float64
-}
-
-// parsePostgresDatabasesStats parses PGResult, extract data and return struct with stats values.
-func parsePostgresDatabasesStats(r *model.PGResult, labelNames []string) map[string]postgresDatabaseStat {
-	log.Debug("parse postgres database stats")
-
-	var stats = make(map[string]postgresDatabaseStat)
-
-	// process row by row
-	for _, row := range r.Rows {
-		stat := postgresDatabaseStat{}
-
-		// collect label values
-		for i, colname := range r.Colnames {
-			switch string(colname.Name) {
-			case "datname":
-				stat.datname = row[i].String
-			}
-		}
-
-		// Define a map key as a database name.
-		databaseFQName := stat.datname
-
-		// Put stats with labels (but with no data values yet) into stats store.
-		stats[databaseFQName] = stat
-
-		// fetch data values from columns
-		for i, colname := range r.Colnames {
-			// skip columns if its value used as a label
-			if stringsContains(labelNames, string(colname.Name)) {
-				log.Debugf("skip label mapped column '%s'", string(colname.Name))
-				continue
-			}
-
-			// Skip empty (NULL) values.
-			if !row[i].Valid {
-				continue
-			}
-
-			// Get data value and convert it to float64 used by Prometheus.
-			v, err := strconv.ParseFloat(row[i].String, 64)
-			if err != nil {
-				log.Errorf("invalid input, parse '%s' failed: %s; skip", row[i].String, err)
-				continue
-			}
-
-			// Run column-specific logic
-			switch string(colname.Name) {
-			case "xact_commit":
-				s := stats[databaseFQName]
-				s.xactcommit = v
-				stats[databaseFQName] = s
-			case "xact_rollback":
-				s := stats[databaseFQName]
-				s.xactrollback = v
-				stats[databaseFQName] = s
-			case "blks_read":
-				s := stats[databaseFQName]
-				s.blksread = v
-				stats[databaseFQName] = s
-			case "blks_hit":
-				s := stats[databaseFQName]
-				s.blkshit = v
-				stats[databaseFQName] = s
-			case "tup_returned":
-				s := stats[databaseFQName]
-				s.tupreturned = v
-				stats[databaseFQName] = s
-			case "tup_fetched":
-				s := stats[databaseFQName]
-				s.tupfetched = v
-				stats[databaseFQName] = s
-			case "tup_inserted":
-				s := stats[databaseFQName]
-				s.tupinserted = v
-				stats[databaseFQName] = s
-			case "tup_updated":
-				s := stats[databaseFQName]
-				s.tupupdated = v
-				stats[databaseFQName] = s
-			case "tup_deleted":
-				s := stats[databaseFQName]
-				s.tupdeleted = v
-				stats[databaseFQName] = s
-			case "conflicts":
-				s := stats[databaseFQName]
-				s.conflicts = v
-				stats[databaseFQName] = s
-			case "temp_files":
-				s := stats[databaseFQName]
-				s.tempfiles = v
-				stats[databaseFQName] = s
-			case "temp_bytes":
-				s := stats[databaseFQName]
-				s.tempbytes = v
-				stats[databaseFQName] = s
-			case "deadlocks":
-				s := stats[databaseFQName]
-				s.deadlocks = v
-				stats[databaseFQName] = s
-			case "blk_read_time":
-				s := stats[databaseFQName]
-				s.blkreadtime = v
-				stats[databaseFQName] = s
-			case "blk_write_time":
-				s := stats[databaseFQName]
-				s.blkwritetime = v
-				stats[databaseFQName] = s
-			case "size_bytes":
-				s := stats[databaseFQName]
-				s.sizebytes = v
-				stats[databaseFQName] = s
-			case "stats_age_seconds":
-				s := stats[databaseFQName]
-				s.statsage = v
-				stats[databaseFQName] = s
-			default:
-				log.Debugf("unsupported pg_stat_database stat column: %s, skip", string(colname.Name))
-				continue
-			}
-		}
-	}
-
-	return stats
-}
-
-// xidLimitStats describes how many XIDs left before force database shutdown due to XID wraparound.
-type xidLimitStats struct {
-	database float64 // based on pg_database.datfrozenxid and datminmxid
-	prepared float64 // based on pg_prepared_xacts.transaction
-	replSlot float64 // based on pg_replication_slots.xmin and catalog_xmin
-}
-
-// parsePostgresXidLimitStats parses database response and returns xidLimitStats.
-func parsePostgresXidLimitStats(r *model.PGResult) xidLimitStats {
-	log.Debug("parse postgres xid limit stats")
-
-	var stats xidLimitStats
-
-	// process row by row
-	for _, row := range r.Rows {
-		// Get data value and convert it to float64 used by Prometheus.
-		value, err := strconv.ParseFloat(row[1].String, 64)
-		if err != nil {
-			log.Errorf("invalid input, parse '%s' failed: %s; skip", row[1].String, err)
-			continue
-		}
-
-		switch row[0].String {
-		case "database":
-			stats.database = value
-		case "prepared_xacts":
-			stats.prepared = value
-		case "replication_slots":
-			stats.replSlot = value
-		}
-	}
-
-	return stats
 }
--- internal/collector/postgres_database_test.go
+++ internal/collector/postgres_database_test.go
@@ -1,9 +1,6 @@
 package collector
 
 import (
-	"database/sql"
-	"github.com/jackc/pgproto3/v2"
-	"github.com/stretchr/testify/assert"
 	"github.com/weaponry/pgscv/internal/model"
 	"testing"
 )
@@ -29,95 +26,4 @@ func TestPostgresDatabasesCollector_Update(t *testing.T) {
 	}
 
 	pipeline(t, input)
-}
-
-func Test_parsePostgresDatabasesStats(t *testing.T) {
-	var testCases = []struct {
-		name string
-		res  *model.PGResult
-		want map[string]postgresDatabaseStat
-	}{
-		{
-			name: "normal output",
-			res: &model.PGResult{
-				Nrows: 2,
-				Ncols: 18,
-				Colnames: []pgproto3.FieldDescription{
-					{Name: []byte("datname")},
-					{Name: []byte("xact_commit")}, {Name: []byte("xact_rollback")}, {Name: []byte("blks_read")}, {Name: []byte("blks_hit")},
-					{Name: []byte("tup_returned")}, {Name: []byte("tup_fetched")}, {Name: []byte("tup_inserted")}, {Name: []byte("tup_updated")}, {Name: []byte("tup_deleted")},
-					{Name: []byte("conflicts")}, {Name: []byte("temp_files")}, {Name: []byte("temp_bytes")}, {Name: []byte("deadlocks")},
-					{Name: []byte("blk_read_time")}, {Name: []byte("blk_write_time")}, {Name: []byte("size_bytes")}, {Name: []byte("stats_age_seconds")},
-				},
-				Rows: [][]sql.NullString{
-					{
-						{String: "testdb1", Valid: true},
-						{String: "100", Valid: true}, {String: "5", Valid: true}, {String: "10000", Valid: true}, {String: "845785", Valid: true},
-						{String: "758", Valid: true}, {String: "542", Valid: true}, {String: "452", Valid: true}, {String: "174", Valid: true}, {String: "125", Valid: true},
-						{String: "33", Valid: true}, {String: "41", Valid: true}, {String: "85642585", Valid: true}, {String: "25", Valid: true},
-						{String: "542542", Valid: true}, {String: "150150", Valid: true}, {String: "485254752", Valid: true}, {String: "4589", Valid: true},
-					},
-					{
-						{String: "testdb2", Valid: true},
-						{String: "254", Valid: true}, {String: "41", Valid: true}, {String: "4853", Valid: true}, {String: "48752", Valid: true},
-						{String: "7856", Valid: true}, {String: "4254", Valid: true}, {String: "894", Valid: true}, {String: "175", Valid: true}, {String: "245", Valid: true},
-						{String: "26", Valid: true}, {String: "84", Valid: true}, {String: "125784686", Valid: true}, {String: "11", Valid: true},
-						{String: "458751", Valid: true}, {String: "235578", Valid: true}, {String: "856964774", Valid: true}, {String: "6896", Valid: true},
-					},
-				},
-			},
-			want: map[string]postgresDatabaseStat{
-				"testdb1": {
-					datname: "testdb1", xactcommit: 100, xactrollback: 5, blksread: 10000, blkshit: 845785,
-					tupreturned: 758, tupfetched: 542, tupinserted: 452, tupupdated: 174, tupdeleted: 125,
-					conflicts: 33, tempfiles: 41, tempbytes: 85642585, deadlocks: 25,
-					blkreadtime: 542542, blkwritetime: 150150, sizebytes: 485254752, statsage: 4589,
-				},
-				"testdb2": {
-					datname: "testdb2", xactcommit: 254, xactrollback: 41, blksread: 4853, blkshit: 48752,
-					tupreturned: 7856, tupfetched: 4254, tupinserted: 894, tupupdated: 175, tupdeleted: 245,
-					conflicts: 26, tempfiles: 84, tempbytes: 125784686, deadlocks: 11,
-					blkreadtime: 458751, blkwritetime: 235578, sizebytes: 856964774, statsage: 6896,
-				},
-			},
-		},
-	}
-
-	for _, tc := range testCases {
-		t.Run(tc.name, func(t *testing.T) {
-			got := parsePostgresDatabasesStats(tc.res, []string{"datname"})
-			assert.EqualValues(t, tc.want, got)
-		})
-	}
-}
-
-func Test_parsePostgresXidLimitStats(t *testing.T) {
-	var testCases = []struct {
-		name string
-		res  *model.PGResult
-		want xidLimitStats
-	}{
-		{
-			name: "normal output",
-			res: &model.PGResult{
-				Nrows:    3,
-				Ncols:    2,
-				Colnames: []pgproto3.FieldDescription{{Name: []byte("src")}, {Name: []byte("to_limit")}},
-				Rows: [][]sql.NullString{
-					{{String: "database", Valid: true}, {String: "2145794333", Valid: true}},
-					{{String: "prepared_xacts", Valid: true}, {String: "2147483647", Valid: true}},
-					{{String: "replication_slots", Valid: true}, {String: "1845258812", Valid: true}},
-					{{String: "invalid", Valid: true}, {String: "invalid", Valid: true}}, // this should be ignored, but logged
-				},
-			},
-			want: xidLimitStats{database: 2145794333, prepared: 2147483647, replSlot: 1845258812},
-		},
-	}
-
-	for _, tc := range testCases {
-		t.Run(tc.name, func(t *testing.T) {
-			got := parsePostgresXidLimitStats(tc.res)
-			assert.EqualValues(t, tc.want, got)
-		})
-	}
 }
