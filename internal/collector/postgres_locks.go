12a8b0621f114d9dfd8812f2cdb4c884708317e3
Alexey Lesovsky <lesovsky@gmail.com>
2021-05-15 14:38:18 +0500

    Use updated common collector logic in postgres/locks collector. Add postgres_locks_all_in_flight, postgres_locks_not_granted_in_flight metrics.

--- internal/collector/postgres_locks.go
+++ internal/collector/postgres_locks.go
@@ -2,88 +2,88 @@ package collector
 
 import (
 	"github.com/prometheus/client_golang/prometheus"
-	"github.com/weaponry/pgscv/internal/log"
 	"github.com/weaponry/pgscv/internal/model"
-	"github.com/weaponry/pgscv/internal/store"
-	"strconv"
-)
-
-const (
-	postgresLocksQuery = "SELECT mode, count(*) FROM pg_locks GROUP BY mode"
 )
 
 // postgresLocksCollector is a collector with locks related metrics descriptors.
 type postgresLocksCollector struct {
-	modes typedDesc
+	builtin []typedDescSet
+	custom  []typedDescSet
 }
 
 // NewPostgresLocksCollector creates new postgresLocksCollector.
 func NewPostgresLocksCollector(constLabels prometheus.Labels, settings model.CollectorSettings) (Collector, error) {
-	return &postgresLocksCollector{
-		modes: typedDesc{
-			desc: prometheus.NewDesc(
-				prometheus.BuildFQName("postgres", "locks", "in_flight"),
-				"Number of in-flight locks held by active processes in each mode.",
-				[]string{"mode"}, constLabels,
-			), valueType: prometheus.GaugeValue,
+	const locksQuery = "SELECT " +
+		"count(*) FILTER (WHERE mode = 'AccessShareLock') AS access_share_lock, " +
+		"count(*) FILTER (WHERE mode = 'RowShareLock') AS row_share_lock, " +
+		"count(*) FILTER (WHERE mode = 'RowExclusiveLock') AS row_exclusive_lock, " +
+		"count(*) FILTER (WHERE mode = 'ShareUpdateExclusiveLock') AS share_update_exclusive_lock, " +
+		"count(*) FILTER (WHERE mode = 'ShareLock') AS share_lock, " +
+		"count(*) FILTER (WHERE mode = 'ShareRowExclusiveLock') AS share_row_exclusive_lock, " +
+		"count(*) FILTER (WHERE mode = 'ExclusiveLock') AS exclusive_lock, " +
+		"count(*) FILTER (WHERE mode = 'AccessExclusiveLock') AS access_exclusive_lock, " +
+		"count(*) FILTER (WHERE not granted) AS not_granted, " +
+		"count(*) AS total " +
+		"FROM pg_locks"
+
+	builtinSubsystems := model.Subsystems{
+		"locks": {
+			Query: locksQuery,
+			Metrics: model.Metrics{
+				{
+					ShortName:   "all_in_flight",
+					Usage:       "GAUGE",
+					Value:       "total",
+					Description: "Total number of in-flight locks held by all active processes.",
+				},
+				{
+					ShortName:   "not_granted_in_flight",
+					Usage:       "GAUGE",
+					Value:       "not_granted",
+					Description: "Total number of not granted in-flight locks held by all active processes.",
+				},
+				{
+					ShortName: "in_flight",
+					Usage:     "GAUGE",
+					LabeledValues: map[string][]string{
+						"mode": {
+							"access_share_lock/AccessShareLock",
+							"row_share_lock/RowShareLock",
+							"row_exclusive_lock/RowExclusiveLock",
+							"share_update_exclusive_lock/ShareUpdateExclusiveLock",
+							"share_lock/ShareLock",
+							"share_row_exclusive_lock/ShareRowExclusiveLock",
+							"exclusive_lock/ExclusiveLock",
+							"access_exclusive_lock/AccessExclusiveLock",
+						},
+					},
+					Description: "Number of in-flight locks held by active processes in each mode.",
+				},
+			},
 		},
+	}
+
+	removeCollisions(builtinSubsystems, settings.Subsystems)
+
+	return &postgresLocksCollector{
+		builtin: newDeskSetsFromSubsystems("postgres", builtinSubsystems, constLabels),
+		custom:  newDeskSetsFromSubsystems("postgres", settings.Subsystems, constLabels),
 	}, nil
 }
 
 // Update method collects locks metrics.
 func (c *postgresLocksCollector) Update(config Config, ch chan<- prometheus.Metric) error {
-	conn, err := store.New(config.ConnString)
-	if err != nil {
-		return err
-	}
-	defer conn.Close()
-
-	// get pg_stat_activity stats
-	res, err := conn.Query(postgresLocksQuery)
+	// Update builtin metrics.
+	err := updateAllDescSets(config, c.builtin, ch)
 	if err != nil {
 		return err
 	}
 
-	// parse pg_stat_activity stats
-	stats := parsePostgresLocksStats(res)
-
-	for mode, value := range stats {
-		ch <- c.modes.mustNewConstMetric(value, mode)
+	// Update user-defined metrics.
+	err = updateAllDescSets(config, c.custom, ch)
+	if err != nil {
+		return err
 	}
 
 	return nil
-}
-
-// parsePostgresLocksStats parses result returned from Postgres and return stats map.
-func parsePostgresLocksStats(r *model.PGResult) map[string]float64 {
-	log.Debug("parse postgres locks stats")
-
-	stats := map[string]float64{
-		"AccessShareLock":          0,
-		"RowShareLock":             0,
-		"RowExclusiveLock":         0,
-		"ShareUpdateExclusiveLock": 0,
-		"ShareLock":                0,
-		"ShareRowExclusiveLock":    0,
-		"ExclusiveLock":            0,
-		"AccessExclusiveLock":      0,
-	}
-
-	for _, row := range r.Rows {
-		if len(row) != 2 {
-			log.Warn("invalid input: wrong number of columns, skip")
-		}
-
-		mode := row[0].String
-
-		v, err := strconv.ParseFloat(row[1].String, 64)
-		if err != nil {
-			log.Errorf("invalid input, parse '%s' failed: %s; skip", row[1].String, err)
-			continue
-		}
-
-		stats[mode] = v
-	}
-
-	return stats
 }
--- internal/collector/postgres_locks_test.go
+++ internal/collector/postgres_locks_test.go
@@ -1,9 +1,6 @@
 package collector
 
 import (
-	"database/sql"
-	"github.com/jackc/pgproto3/v2"
-	"github.com/stretchr/testify/assert"
 	"github.com/weaponry/pgscv/internal/model"
 	"testing"
 )
@@ -11,51 +8,13 @@ import (
 func TestPostgresLocksCollector_Update(t *testing.T) {
 	var input = pipelineInput{
 		required: []string{
+			"postgres_locks_all_in_flight",
 			"postgres_locks_in_flight",
+			"postgres_locks_not_granted_in_flight",
 		},
 		collector: NewPostgresLocksCollector,
 		service:   model.ServiceTypePostgresql,
 	}
 
 	pipeline(t, input)
-}
-
-func Test_parsePostgresLocksStats(t *testing.T) {
-	var testcases = []struct {
-		name string
-		res  *model.PGResult
-		want map[string]float64
-	}{
-		{
-			name: "normal output",
-			res: &model.PGResult{
-				Nrows:    4,
-				Ncols:    2,
-				Colnames: []pgproto3.FieldDescription{{Name: []byte("mode")}, {Name: []byte("count")}},
-				Rows: [][]sql.NullString{
-					{{String: "RowExclusiveLock", Valid: true}, {String: "150", Valid: true}},
-					{{String: "RowShareLock", Valid: true}, {String: "100", Valid: true}},
-					{{String: "ExclusiveLock", Valid: true}, {String: "50", Valid: true}},
-					{{String: "AccessShareLock", Valid: true}, {String: "2000", Valid: true}},
-				},
-			},
-			want: map[string]float64{
-				"AccessShareLock":          2000,
-				"RowShareLock":             100,
-				"RowExclusiveLock":         150,
-				"ShareUpdateExclusiveLock": 0,
-				"ShareLock":                0,
-				"ShareRowExclusiveLock":    0,
-				"ExclusiveLock":            50,
-				"AccessExclusiveLock":      0,
-			},
-		},
-	}
-
-	for _, tc := range testcases {
-		t.Run(tc.name, func(t *testing.T) {
-			got := parsePostgresLocksStats(tc.res)
-			assert.EqualValues(t, tc.want, got)
-		})
-	}
 }
